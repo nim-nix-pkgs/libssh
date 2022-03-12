dcbb803501a285eaf747654a88d16e4871f6a12c
hugosenari <hugosenari@gmail.com>
2022-03-12 02:58:29 -0300

    chore: re index libssh

---
+++ 0_90_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libssh FFI bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libssh-0_90_0.flake = false;
+  inputs.src-libssh-0_90_0.owner = "dariolah";
+  inputs.src-libssh-0_90_0.ref   = "refs/tags/0.90.0";
+  inputs.src-libssh-0_90_0.repo  = "libssh-nim";
+  inputs.src-libssh-0_90_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libssh-0_90_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libssh-0_90_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libssh FFI bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libssh-master.flake = false;
+  inputs.src-libssh-master.owner = "dariolah";
+  inputs.src-libssh-master.ref   = "refs/heads/master";
+  inputs.src-libssh-master.repo  = "libssh-nim";
+  inputs.src-libssh-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libssh-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libssh-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
